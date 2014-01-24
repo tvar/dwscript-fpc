4834590fb3dea3cf2f66d5d6a30523777b12eab2
Роман Зотов <zotov@oe-it.ru>
2014-01-24 15:39:16 +0400

    lpr removed, fpc and delphi use common dpr file

--- Test/LanguageTests.dpr
+++ Test/LanguageTests.dpr
@@ -1,17 +1,26 @@
 program LanguageTests;
 
-{$SetPEFlags $0001}
+{$I dws.inc}
+{$IFDEF WINDOWS}
+  {$SetPEFlags $0001}
+{$ENDIF}
 
+{$IFNDEF FPC}
 {$IFNDEF VER200}
 {.$WEAKLINKRTTI ON}
 {$RTTI EXPLICIT METHODS([]) PROPERTIES([]) FIELDS([])}
 {$ENDIF}
+{$ENDIF}
 
 uses
+{$IFDEF FPC}
+  {$IFDEF WINDOWS} Windows, {$ELSE} cthreads, cmem, {$ENDIF}
+  LCLIntf, LCLType, LMessages, Interfaces,
+{$ELSE}
+  TestFrameWork, Windows,
+{$ENDIF}
   Classes,
   Forms,
-  Windows,
-  TestFrameWork,
   GUITestRunner,
   SysUtils,
   dwsXPlatform,
@@ -30,9 +39,7 @@ uses
   UCornerCasesTests in 'UCornerCasesTests.pas',
   UdwsClassesTests in 'UdwsClassesTests.pas',
   dwsClasses in '..\Libraries\ClassesLib\dwsClasses.pas',
-  UdwsDataBaseTests in 'UdwsDataBaseTests.pas',
   UdwsFunctionsTests in 'UdwsFunctionsTests.pas',
-  UCOMConnectorTests in 'UCOMConnectorTests.pas',
   UTestDispatcher in 'UTestDispatcher.pas',
   UDebuggerTests in 'UDebuggerTests.pas',
   UdwsUtilsTests in 'UdwsUtilsTests.pas',
@@ -46,31 +53,43 @@ uses
   UTokenizerTests in 'UTokenizerTests.pas',
   ULanguageExtensionTests in 'ULanguageExtensionTests.pas',
   UJITTests in 'UJITTests.pas',
+{$IFDEF CPU32}
+  {$IFDEF WINDOWS}
   UJITx86Tests in 'UJITx86Tests.pas',
+  {$ENDIF}
+{$ENDIF}
+{$IFNDEF FPC}
   ULinqTests in 'ULinqTests.pas',
+  UCOMConnectorTests in 'UCOMConnectorTests.pas',
+  UdwsDataBaseTests in 'UdwsDataBaseTests.pas',
 {$IF RTLVersion >= 21}
   dwsSynSQLiteDatabase in '..\Libraries\DatabaseLib\dwsSynSQLiteDatabase.pas',
   dwsUIBDataBase,
   URTTIExposeTests in 'URTTIExposeTests.pas',
   USpecialTestsRTTI in 'USpecialTestsRTTI.pas',
 {$IFEND}
+{$ENDIF}
   ULinqJsonTests in 'ULinqJsonTests.pas';
-
-{$R *.res}
-
+{$IFDEF FPC}
+  procedure RunRegisteredTests;
+  begin
+    Application.CreateForm(TGUITestRunner, TestRunner);
+  end;
+{$ENDIF}
+{$IFNDEF FPC}
 var
-{$IF RTLVersion >= 23}
-   procAffinity, systAffinity : NativeUInt;
-{$ELSE}
-   procAffinity, systAffinity : DWORD;
-{$IFEND}
+   procAffinity, systAffinity : {$IF RTLVersion >= 23} NativeUInt;{$ELSE}DWORD;{$IFEND}
+{$ENDIF}
 begin
    DirectSet8087CW($133F);
+   {$IFNDEF FPC}
    GetProcessAffinityMask(GetCurrentProcess, procAffinity, systAffinity);
    SetProcessAffinityMask(GetCurrentProcess, systAffinity);
-   SetDecimalSeparator('.');
    ReportMemoryLeaksOnShutdown:=True;
+   {$ENDIF}
+   SetDecimalSeparator('.');
    Application.Initialize;
-   GUITestRunner.RunRegisteredTests;
+   RunRegisteredTests;
+   Application.Run;
 end.
 
